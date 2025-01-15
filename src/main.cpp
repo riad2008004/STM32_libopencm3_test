70e81ef83ce0daf735d8572f84885a3c4bf96298
riad2008004 <arifulislamriad5@gmail.com>
2025-01-16 04:23:00 +0600

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 
-void delay(int count) {
-    for (int i = 0; i < count; i++) {
-        __asm__("nop");
+void delay(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        __asm__("nop"); // add comment
     }
 }
 
-int main(void) {
+int main(void)
+{
     // Enable GPIOC clock
     rcc_periph_clock_enable(RCC_GPIOC);
 
     // Set GPIOC pin 13 as output
-    gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_10_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO13);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_10_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO13);
 
-    while (1) {
+    while (1)
+    {
         // Toggle the LED
         gpio_toggle(GPIOC, GPIO13);
 
